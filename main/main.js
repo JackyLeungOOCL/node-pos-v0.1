e137a4fb12f6bafee1d3324deab541293a4c5ed9
jacky.leung <jacky.leung@oocl.com>
2018-11-19 17:47:50 +0800

    not yet complete

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
+'use strict'
+function printReceipt(inputs) {
+	var string = "***<store earning no money>Receipt ***\n";
+	var list;
+	inputs.forEach(function(inp) {
+		list.forEach(function(item) {
+			if () {
+				
+			}
+		});
+	});
+}
+
 module.exports = function main() {
     console.log("Debug Info");
+
     return 'Hello World!';
 };
